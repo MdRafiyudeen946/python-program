5e541897df1452fc05a75db738732b90c3d7a666
MdRafiyudeen946 <mdrafiyudeen946@gmail.com>
2022-09-14 07:34:29 +0000

    all

---
+++ hello1.py
@@ -0,0 +1,22 @@
+import prefect
+from prefect import task, Flow
+from datetime import timedelta
+from prefect.schedules import IntervalSchedule
+from prefect.storage import GitHub
+
+@task
+def hello_task():
+    logger = prefect.context.get("logger")
+    logger.info("Hello world!")
+
+schedule = IntervalSchedule(interval=timedelta(minutes=1))
+
+with Flow("hello-flow",schedule=schedule) as flow:
+    hello_task()
+
+flow.register(project_name="Project")
+
+flow.storage = GitHub(
+        repo="python-program",
+        path="hello1.py"
+)
