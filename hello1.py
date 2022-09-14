2d9d33df89ac3adf3f5a83e30f877e12b51beb32
MdRafiyudeen946 <mdrafiyudeen946@gmail.com>
2022-09-14 13:18:23 +0000

    a

--- hello1.py
+++ hello1.py
@@ -10,16 +10,17 @@ def hello_task():
     logger.info("Hello world!")
 
 schedule = IntervalSchedule(interval=timedelta(minutes=1))
-with Flow("hello-flow111",schedule=schedule) as flow:
+with Flow("hello-flow11",schedule=schedule) as flow:
 
 #with Flow("hello-flow111") as flow:
     hello_task()
 
-#flow.register(project_name="project123")
+flow.register(project_name="project123")
 
 flow.storage = GitHub(
         repo="python-program",
         path="hello1.py",
 	access_token_secret="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"
 )
+
 
